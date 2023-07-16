43e50b57577b8699c80bb38c9f1fb6f937104aed
bliona <lionamuhati16@gmail.com>
2023-07-16 07:47:42 -0700

    AirBnB

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""__init__ magic directory method for models"""
+starting at models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Outlines the Amenity class."""
+starting at models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Defines an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
+    name = ""
